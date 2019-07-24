5c16b827066d589c84e3a59985952aaccab6c707
github cjl248 <github cjl248@cornell.edu>
2019-07-24 10:36:59 -0400

    doneee

--- tools/console.rb
+++ tools/console.rb
@@ -4,8 +4,35 @@ def reload
   load 'config/environment.rb'
 end
 
-
 ###  WRITE YOUR TEST CODE HERE ###
+
+author1 = Author.new("chris")
+author2 = Author.new("elisa")
+author3 = Author.new("beatlejuice")
+
+
+m1 = Magazine.new("legos", "toys")
+m2 = Magazine.new("k'nex", "toys")
+m3 = Magazine.new("play-dough", "mess-makers")
+m4 = Magazine.new("play-boy", "adult")
+
+
+article1 = Article.new(author1, m1, "this lego trick TERRIFIES lego masters", "should have highest word word word word word count")
+
+article2 = Article.new(author3, m4, "SAY IT ONE MORE TIME!", "art tickle content content")
+
+article3 = Article.new(author2, m4, "play-dough is phun!", "art tickle content content content")
+
+articl4 = Article.new(author3, m1, "LEGOOO SWOOOP", "art tickle content content content content")
+
+articl5 = Article.new(author2, m2, "title5", "art tickle content content content content content")
+
+articl6 = Article.new(author1, m3, "title6", "art tickle content content content content content content")
+
+
+
+
+
 
 
 
